4ae71ccae7da8f4d91aaab581291997d71152df5
Michelle Mismas <michelle.mismas@gmail.com>
2024-07-16 12:28:05 -0400

    before lunch day 2

--- frontend/src/app/components/nav-bar/components/user-menu.component.ts
+++ frontend/src/app/components/nav-bar/components/user-menu.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-user-menu',
   standalone: true,
-  imports: [],
-  template: ` <a class="btn">Button</a> `,
+  imports: [AsyncPipe],
+  template: ` <div class="btn">{{ (user$ | async)?.sub }}</div> `,
   styles: ``,
 })
-export class UserMenuComponent {}
+export class UserMenuComponent {
+  client = inject(HttpClient);
+  user$ = this.client.get<{ sub: string }>('/api/user');
+}
